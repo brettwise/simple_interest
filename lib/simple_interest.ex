312325fcf9d48100d67e000e5a74c0c3dbc2c5d2
Brett Wise <brettdwise@gmail.com>
2016-02-08 13:02:33 -0500

    completed first part of challenge with tests

--- lib/simple_interest.ex
+++ lib/simple_interest.ex
@@ -1,2 +1,41 @@
 defmodule SimpleInterest do
+  def main do
+    principal =
+      get_number("principal")
+      |>convert_to_float
+
+    rate      =
+      get_number("rate of interest")
+      |>convert_to_float
+
+    years     =
+    get_number("number of years")
+      |>convert_to_float
+
+    return = calc_ROI(principal, rate, years)
+    send_message(years, rate, return)
+  end
+
+  def get_number(string) do
+    IO.gets "Enter the #{string}: "
+  end
+
+  def convert_to_float(string) do
+    try do
+      {float, _} = Float.parse(string)
+      float
+    rescue
+      _ ->
+        IO.puts "Whoa mang, that doesn't look like a number. Let's try again."
+        main
+    end
+  end
+
+  def calc_ROI(principal, rate, years) do
+    (principal * (1 + ((rate/100) * years)))
+  end
+
+  def send_message(years, rate, return) do
+    IO.puts "After #{round(years)} at #{rate}%, the investment will be worth $#{Float.round(return, 2)}"
+  end
 end
--- test/simple_interest_test.exs
+++ test/simple_interest_test.exs
@@ -1,8 +1,7 @@
 defmodule SimpleInterestTest do
   use ExUnit.Case
-  doctest SimpleInterest
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "pass in principal, rate, and years as 3 floats and return ROI + principal" do
+    assert SimpleInterest.calc_ROI(1500, 4.3, 4) == 1758.0
   end
 end
